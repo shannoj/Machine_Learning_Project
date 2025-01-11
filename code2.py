9e725dcc302fda080a5f575d596ba85b23faed34
shannoj <69207315+shannoj@users.noreply.github.com>
2025-01-11 12:10:26 -0800

    imported pytorch and yfinance

---
+++ code2.py
@@ -0,0 +1,35 @@
+import torch
+import yfinance as yf
+from datetime import date
+
+end_date = date.today().strftime("%Y-%m-%d")
+start_date = '1990-01-01'
+
+df = yf.download('AAPL', start=start_date, end=end_date)
+
+import seaborn as sns
+from pylab import rcParams
+import matplotlib.pyplot as plt
+from matplotlib import rc
+
+import pandas as pd
+import numpy as np
+from tqdm.notebook import tqdm
+
+from sklearn.preprocessing import MinMaxScaler
+
+import torch
+import torch.nn as nn
+import torch.optim as optim
+from torch.utils.data import DataLoader, TensorDataset
+from sklearn.metrics import mean_squared_error, r2_score
+from collections import defaultdict
+
+%matplotlib inline
+
+sns.set(style='whitegrid', palette='muted', font_scale=1.2)
+
+Colour_Palette = ['#01BEFE', '#FF7D00', '#FFDD00', '#FF006D', '#ADFF02', '#8F00FF']
+sns.set_palette(sns.color_palette(Colour_Palette))
+
+tqdm.pandas()
