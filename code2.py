30c66f613744105ef485c517881d83daeddee061
shannoj <69207315+shannoj@users.noreply.github.com>
2025-01-11 16:09:02 -0800

    added data plot for apple stock data

--- code2.py
+++ code2.py
@@ -26,3 +26,31 @@ Colour_Palette = ['#01BEFE', '#FF7D00', '#FFDD00', '#FF006D', '#ADFF02', '#8F00F
 sns.set_palette(sns.color_palette(Colour_Palette))
 
 tqdm.pandas()
+
+import yfinance as yf
+from datetime import date
+
+end_date = date.today().strftime("%Y-%m-%d")
+start_date = '1990-01-01'
+
+df = yf.download('AAPL', start=start_date, end=end_date)
+
+import matplotlib.dates as mdates
+
+def data_plot(df):
+    # Plot line charts
+    df_plot = df.copy()
+
+    ncols = 2
+    nrows = int(round(df_plot.shape[1] / ncols, 0))
+
+    fig, ax = plt.subplots(nrows=nrows, ncols=ncols, sharex=True, figsize=(14, 7))
+    for i, ax in enumerate(fig.axes):
+        sns.lineplot(data=df_plot.iloc[:, i], ax=ax)
+        ax.tick_params(axis="x", rotation=30, labelsize=10, length=0)
+        ax.xaxis.set_major_locator(mdates.AutoDateLocator())
+    fig.tight_layout()
+    plt.show()
+
+# Plot the data
+data_plot(df)
